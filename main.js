25447e7e5ff23afb7fa1e08ec272c69a9ba848aa
saitejavunnava3377 <146068496+saitejavunnava3377@users.noreply.github.com>
2023-09-28 14:23:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function stringNumCon(){
+    
+
+let name="saiteja";
+let number=23456;
+   a=(name+" : "+number);
+    document.getElementById("l1").textContent=a;
+    
+
+    
+    }
+
+function Boolean(){
+    const isTodayThursday=true;
+    b=isTodayThursday;
+    document.getElementById("l2").textContent=b;
+}
+
+
+
+
+
+
+
+
+let unDefinedVariable;
+
+console.log(unDefinedVariable===undefined?"variable value is undefined":unDefinedVariable);
+
+
+
+let myArr=["today","yestarday","tommorow"];
+
+console.log(myArr[2]);
